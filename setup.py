1b89920c1edc7c9a625a25c2883369c437fe5a6c
josimarlourencodev-lab <josimarlourencodev@gmail.com>
2026-01-16 10:46:24 -0300

    algum mundaças

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+from cx_Freeze import setup, Executable
+
+#Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "includes": ["tkinter", "customtkinter","pygame","PIL","time","math","threading"]}
+
+'''GUI applications require a different base on Windows (the default is for
+a console application).'''
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="Meu App",
+    version="0.1",
+    description="PyMusic",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("app.py", base=base)]
+)
